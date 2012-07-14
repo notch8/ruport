0e41f88addd93f3e69eb43246de0339a69f4fa0d
Rob Kaufman <rob@notch8.com>
2012-07-13 18:00:10 -0700

    Proper format for the CSV call

--- lib/ruport/formatter/csv.rb
+++ lib/ruport/formatter/csv.rb
@@ -52,7 +52,7 @@ module Ruport
     #
     def csv_writer
       @csv_writer ||= options.formatter ||
-        FCSV(output, options.format_options || {})
+        ::CSV.new(output, options.format_options || {})
     end
 
     # Generates table header by turning column_names into a CSV row.
